f9bb06010231b6aecf15e8ac9009d83d65df4d3a
Badr Ijaz <badrkmc@gmail.com>
2020-05-30 16:51:45 +0500

    created react app

---
+++ react-test/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload. julian is a retard
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;

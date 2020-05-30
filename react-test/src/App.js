cbb00279cb2dde664f1de623fe3e22a602ba6f7a
Badr Ijaz <badrkmc@gmail.com>
2020-05-30 17:16:22 +0500

    cleaning up react

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. julian is a retard
+        
         </p>
         <a
           className="App-link"

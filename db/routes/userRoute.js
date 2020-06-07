860c9222e41aad89502dfa749ca6871a7ead0cd5
Badr Ijaz <badrkmc@gmail.com>
2020-06-07 21:53:37 +0500

    added find user by ID option

--- db/routes/userRoute.js
+++ db/routes/userRoute.js
@@ -16,6 +16,7 @@ router.route('/add').post((req, res) => {
     newUser.save()
     .then(() => res.json(`Registered user ${username}`))
     .catch(err => res.status(400).json('Error: ' + err))
+    
 });
 
 router.route('/').get((req, res) => {

262b545f1ba212d5038d7ccaf45571bc815ec73c
Badr Ijaz <badrkmc@gmail.com>
2020-06-07 21:49:28 +0500

    Fix user finding in question adding

--- db/routes/questionRoute.js
+++ db/routes/questionRoute.js
@@ -2,35 +2,33 @@ const router = require('express').Router();
 let QuestionModel = require('../models/questionModel')
 let UserModel = require('../models/userModel')
 
-router.route('/add').post((req, res) => {
-    
-    let user
-    console.log(req.body.author)
+userExists = async user => {
 
-    UserModel.find({ username: req.body.author })
+    UserModel.find({ username: user })
         .then(users => {
-            
-            user = users[0]
-            
-            if (user) {
-                const newQuestion = new QuestionModel({
-                    title: req.body.title,
-                    content: req.body.content,
-                    author: req.body.author
-                })
-
-                newQuestion.save()
-                .then(() => res.json(`Submitted ${req.body.title}`))
-                .catch(err => res.status(400).json('Error: ' + err))
-            } else {
-                res.status(400).json("Username not found");
-            }
-
+            if (users) {
+                return true
+            } 
         })
-        .catch(err => res.status(400).json('Error: ' + err));
+}
 
+router.route('/add').post((req, res) => {
+    userFound = userExists(req.body.author)
 
-    
+    if (userFound) {
+        const newQuestion = new QuestionModel({
+            title: req.body.title,
+            content: req.body.content,
+            author: req.body.author
+        })
+
+        newQuestion.save()
+        .then(() => res.json(`Submitted ${req.body.title}`))
+        .catch(err => res.status(400).json('Error: ' + err))
+
+    } else {
+        res.status(400).json("User not found");
+    }
 
 });
 

4fcbda6d0f0deb3de651ce173b38593feaec00b5
Julian <julianibibo@gmail.com>
2020-06-10 13:23:14 +0200

    work

--- frontend/src/components/questionsPanel.jsx
+++ frontend/src/components/questionsPanel.jsx
@@ -2,28 +2,35 @@ import React, { Component } from "react";
 
 import axios from "axios";
 
-import Question from "./question";
+// import Question from "./question";
 
 class QuestionsPanel extends Component {
-  state = {};
-  render() {
-    let questionsFormatted;
+  state = {
+    questions: [],
+  };
 
+  render() {
     axios
       .get("http://localhost:3000/questions")
       .then((r) => {
-        r.data.forEach((q) => {
-          <li id={q._id}>
-            <b>{q.title}</b> by {q.author} (id = {q._id})
-          </li>;
+        this.setState({
+          questions: r.data,
         });
       })
       .catch((err) => console.log(err));
 
     return (
       <div className="panel">
-        <h2>Active questions:</h2>
-        <Question />
+        <h2>Questions:</h2>
+        <ul>
+          {this.state.questions.map((question) => {
+            return (
+              <li key={question._id}>
+                {question.title} by {question.author}
+              </li>
+            );
+          })}
+        </ul>
       </div>
     );
   }

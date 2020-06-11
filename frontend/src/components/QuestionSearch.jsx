b485da1ff2ef7e11da6b88e240044d0d9996908f
Badr Ijaz <badrkmc@gmail.com>
2020-06-11 21:45:47 +0500

    added separate question search component

--- frontend/src/components/QuestionSearch.jsx
+++ frontend/src/components/QuestionSearch.jsx
@@ -1,12 +1,47 @@
 import React, { Component } from "react";
 
-import axios from "axios";
-
 class QuestionSearch extends Component {
   // moved to questions panel
-  state = {};
+  state = {
+    searchValue: "",
+  };
+
+  handleChange = (event) => {
+    this.setState(
+      { searchValue: event.target.value },
+      this.props.fetchQuestions(event.target.value)
+    );
+  };
+
+  handleSubmit = (event) => {
+    console.log("Submitted: " + this.state.searchValue);
+    event.preventDefault();
+  };
+
   render() {
-    return 0;
+    return (
+      <div className="row">
+        <div className="col-lg-10 col-md-10">
+          <form onSubmit={this.handleSubmit} autoComplete="off">
+            <input
+              type="text"
+              name="question"
+              className="form-control"
+              onChange={this.handleChange}
+              value={this.state.searchValue}
+              placeholder="Search questions..."
+            />
+          </form>
+        </div>
+        <div className="col-lg-2 col-md-2">
+          <input
+            type="submit"
+            value="Search"
+            className="btn btn-lg btn-primary"
+          />
+        </div>
+      </div>
+    );
   }
 }
 
--- frontend/src/components/QuestionsPanel.jsx
+++ frontend/src/components/QuestionsPanel.jsx
@@ -3,23 +3,16 @@ import React, { Component } from "react";
 import axios from "axios";
 
 import Question from "./Question";
+import QuestionSearch from "./QuestionSearch";
 
 class QuestionsPanel extends Component {
   state = {
     questions: [],
-    searchValue: "",
   };
 
   componentDidMount() {
     this.fetchQuestions();
   }
-
-  handleChange = (event) => {
-    this.setState(
-      { searchValue: event.target.value },
-      this.fetchQuestions(event.target.value)
-    );
-  };
 
   fetchQuestions = (searchValue) => {
     console.log(searchValue);
@@ -32,38 +25,12 @@ class QuestionsPanel extends Component {
       });
   };
 
-  handleSubmit = (event) => {
-    console.log("Submitted: " + this.state.searchValue);
-    event.preventDefault();
-  };
-
   render() {
     return (
       <div className="col-md-12">
         {this.props.showSearchBar ? (
-          <div className="row">
-            <div className="col-lg-10 col-md-10">
-              <form onSubmit={this.handleSubmit} autoComplete="off">
-                <input
-                  type="text"
-                  name="question"
-                  className="form-control"
-                  onChange={this.handleChange}
-                  value={this.state.searchValue}
-                  placeholder="Search questions..."
-                />
-              </form>
-            </div>
-            <div className="col-lg-2 col-md-2">
-              <input
-                type="submit"
-                value="Search"
-                className="btn btn-lg btn-primary"
-              />
-            </div>
-          </div>
+          <QuestionSearch fetchQuestions={this.fetchQuestions} />
         ) : null}
-
         <div className="row">
           <h2>{this.state.searchValue ? "Search results:" : "Questions:"}</h2>
         </div>

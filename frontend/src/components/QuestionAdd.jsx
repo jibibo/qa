02b7e68b95720ed6b9602e2879cac12b4e029ff3
Badr Ijaz <badrkmc@gmail.com>
2020-06-15 23:55:56 +0500

    tags

--- frontend/src/components/QuestionAdd.jsx
+++ frontend/src/components/QuestionAdd.jsx
@@ -1,56 +1,80 @@
 import React, { Component } from "react";
 
 class QuestionAdd extends Component {
-  state = {};
+  state = {
+    title: "",
+    content: "",
+    tag: "",
+    tags: [],
+  };
 
   handleSubmit = (event) => {
     console.log("Submitted search: " + this.state.searchValue);
     event.preventDefault();
   };
 
+  onUpdate = (e) => {
+    if (e.target.name === "tag") {
+      if (e.key === " ") {
+        var tags = this.state.tags.concat(e.target.value);
+        this.setState({ tags: tags });
+        console.log(this.state.tags);
+        e.target.value = null;
+        e.preventDefault();
+      } else {
+        this.setState({ [e.target.name]: e.target.value });
+        console.log(e.target.name, e.target.value);
+      }
+    } else {
+      this.setState({ [e.target.name]: e.target.value });
+    }
+  };
+
   render() {
     return (
       <div id="QuestionAdd" className="col-md-12">
         <form onSubmit={this.handleSubmit}>
-          <div className="row">
+          <div className="form-group">
             <input
               type="text"
               className="form-control"
               name="title"
-              placeholder="Title*"
+              placeholder="Title (required)"
+              value={this.state.title}
+              onChange={this.onUpdate}
             />
           </div>
-          <div className="row">
-            <input
-              type="text"
-              className="form-control"
-              name="author"
-              placeholder="Author*"
-            />
-          </div>
-          <div className="row">
+          <div className="form-group">
             <input
               type="text"
               className="form-control"
               name="content"
               placeholder="Content"
+              value={this.state.content}
+              onChange={this.onUpdate}
             />
           </div>
-          <div className="row">
+          <div className="form-group">
+            <div className="bg-success rounded p-5 mx-10">
+              topic placeholder
+            </div>
+          </div>
+
+          <div className="form-group">
             <input
               type="text"
               className="form-control"
-              name="topics"
-              placeholder="Topic 1, topic 2"
+              name="tag"
+              placeholder="topic 1 topic 2"
+              onKeyPress={this.onUpdate}
             />
           </div>
-          <div className="row">
+          <div className="form-group">
             <input
               type="submit"
               value="Submit question"
               className="btn btn-md btn-primary"
             />
-            <p>* required</p>
           </div>
         </form>
       </div>

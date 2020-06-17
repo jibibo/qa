98c74d9e77dc6f0bb595fda1ccff61a60b409f05
Julian <julianibibo@gmail.com>
2020-06-17 22:37:26 +0200

    temp rename fix

---
+++ backend/routes/q.js
@@ -0,0 +1,70 @@
+const router = require("express").Router();
+let QuestionModel = require("../models/QuestionModel");
+let { sessionTokenValid } = require("../util");
+
+// // Routes
+
+router.route("/search").get((req, res) => {
+  console.log("ROUTE:START /question/search");
+
+  var params = {};
+
+  if (req.query[0] !== undefined) {
+    params = {
+      title: {
+        $regex: req.query[0],
+        $options: "i",
+      },
+    };
+  }
+
+  console.log(`ROUTE:INFO /question/search: searching, params:sa ${params}`);
+
+  QuestionModel.find(params)
+    .then((questions) => {
+      console.log(
+        `ROUTE:OK /question/search: found ${questions.length} matches`
+      );
+      res.status(200).json(questions);
+    })
+    .catch((err) => {
+      console.log(`ROUTE:ERR /question/search: ${err}`);
+      res.status(400).json({ result: "err", err: err });
+    });
+});
+
+router.route("/add").post((req, res) => {
+  console.log("ROUTE:START /question/add");
+
+  sessionTokenValid(req.body.sessionToken, (foundUser) => {
+    console.log(req.body);
+    if (foundUser) {
+      const newQuestion = new QuestionModel({
+        title: req.body.title,
+        description: req.body.description,
+        tags: req.body.tags,
+        authorId: foundUser._id,
+      });
+
+      console.log("ROUTE:INFO /question/add: created new QuestionModel");
+
+      newQuestion
+        .save()
+        .then(() => {
+          console.log(`ROUTE:OK /question/add: added ${req.body.title}`);
+          res.status(200).json({ response: "success", title: req.body.title });
+        })
+        .catch((err) => {
+          console.log(`ROUTE:ERR /question/add: ${err}`);
+          res.status(400).json({ response: "err", err: err });
+        });
+    } else {
+      let err = "Invalid session token";
+
+      console.log(`ROUTE:ERR /question/add: ${err}`);
+      res.status(400).json({ response: "err", err: err });
+    }
+  });
+});
+
+module.exports = router;
